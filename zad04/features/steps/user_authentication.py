1e6e6ac08a73119168a8b6368071a9693298d7c8
Adrian Stokwisz <stokwiszadrian@gmail.com>
2022-01-25 19:47:28 +0100

    'zad04'

--- zad04/features/steps/user_authentication.py
+++ zad04/features/steps/user_authentication.py
@@ -1,15 +1,13 @@
-import time
-import unittest
 from selenium import webdriver
 from behave import *
 
 use_step_matcher("re")
 
-@given("the website wp.pl")
+@given("the website facebook.com")
 def step_impl(context):
     context.driver = webdriver.Chrome(executable_path="../drivers/chromedriver")
     context.driver.implicitly_wait(10)
-    context.driver.get("https://www.wp.pl/")
+    context.driver.get("https://facebook.com/")
 
 @given("email address (?P<email>.+)")
 def step_impl(context, email):
@@ -21,12 +19,21 @@ def step_impl(context, password):
 
 @when("attempting to log in")
 def step_impl(context):
-    context.driver.execute_script("arguments[0].click()", context.driver.find_element(by="id", value="ol-widget-login-button"))
+    # context.driver.execute_script("arguments[0].click()", context.driver.find_element(by="id", value="u_0_j_3b"))
     context.url_before = context.driver.current_url
-    context.driver.find_element(by="id", value="login").send_keys(context.email)
-    context.driver.find_element(by="id", value="password").send_keys(context.password)
+    if context.driver.current_url == "https://www.facebook.com/user_cookie_prompt/":
+        context.driver.find_element(by="xpath", value="//div[@aria-label='Allow All Cookies']").click()
+    context.driver.find_element(by="id", value="email").send_keys(context.email)
+    context.driver.find_element(by="id", value="pass").send_keys(context.password)
     context.driver.find_element(by="xpath", value="//button[@type='submit']").submit()
 
 @then("it fails")
 def step_impl(context):
-    assert len(context.driver.find_elements(by="xpath", value="//div[@data-testid='login-error']")) != 0
+    assert context.driver.current_url != "https://www.facebook.com/?sk=welcome"
+
+@then("user successfuly logs in")
+def step_impl(context):
+    print(context.driver.current_url)
+    print(context.driver.find_elements(by="xpath", value="//input[@aria-label='Search Facebook']")[0].text)
+    assert context.driver.current_url == "https://www.facebook.com/?sk=welcome"
+
--- zad04/features/user_authorization.feature
+++ zad04/features/user_authorization.feature
@@ -1,16 +1,34 @@
 Feature: User authorization
+  Fikcyjne konta zostały utworzone z pomocą aplikacji testowych Facebooka
+  https://developers.facebook.com/docs/development/build-and-test/test-users
 
-  Scenario Outline: User login to wp.pl
-    Given the website wp.pl
+  Scenario Outline: login success
+    Given the website facebook.com
+    And email address <email>
+    And password <password>
+    When attempting to log in
+    Then user successfuly logs in
+
+    Examples:
+    | email                           | password         |
+    | ipuujgedzh_1643133981@tfbnw.net | zaq1@WSX         |
+    | eerssbqvjw_1643133981@tfbnw.net | Q@wertyuiop      |
+    | skmrkoqecq_1643133981@tfbnw.net | strongpa$$word   |
+    | rmgpwisopn_1643133981@tfbnw.net | f!rewall         |
+    | ykbxlzipmg_1643130920@tfbnw.net | zaq1@WSX         |
+
+  Scenario Outline: login fail
+    Given the website facebook.com
     And email address <email>
     And password <password>
     When attempting to log in
     Then it fails
 
     Examples:
-    | email              | password         |
-    | abc@example.pl     | zaq1@WSX         |
-    | efgh@example.pl    | pa$$word         |
-    | abc@example.pl     | firewa!!         |
-    | test@example.pl    | 12345678         |
+    | email                           | password         |
+    | abc@example.com                 | zaq1@WSX         |
+    | randommail@example.com          | Q@wertyuiop      |
+    | lkasjflaksjf@example.com        | strongpa$$word   |
+    | mammamia@example.com            | f!rewall         |
+    | test@example.com                | zaq1@WSX         |
 
